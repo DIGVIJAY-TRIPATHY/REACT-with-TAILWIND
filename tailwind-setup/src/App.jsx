2f358ee1c4de3e2f2cc394bca52f7058ab364e99
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-05-30 20:59:41 +0530

    tailwind installation and using done

---
+++ tailwind-setup/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-white p-4 rounded-2xl'>tailwind test</h1>
+    </>
+  )
+}
+
+export default App
