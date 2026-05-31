22318d948a32c878cab5b6dc7032f718cb716dcf
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-05-31 21:27:55 +0530

    learning props

--- tailwind-setup/src/App.jsx
+++ tailwind-setup/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
-
+import Card from './components/Card'
 
 function App() {
-
   return (
     <>
-      <h1 className='bg-green-400 text-white p-4 rounded-2xl'>tailwind test</h1>
-
-      
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="rudra" btnText="click me" />
+      <Card username="digvijay" />
     </>
   )
 }
